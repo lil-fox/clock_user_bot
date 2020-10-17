a4aeb8038b996ef0e236d37d2da9a3ca8a79e2bc
fox-dever <69247125+fox-dever@users.noreply.github.com>
2020-10-17 13:32:54 +0300

    add config

---
+++ config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+BOT_ID = "clock_bot"
+
+TIME_ZOME = "Europe/Kiev"
+
+DIR_PATH = Path(__file__).parent.absolute()
+CLOCK_SOURCE_PATH = f"{DIR_PATH}/clock.psd"
+CLOCK_PATH = f"{DIR_PATH}/next.png"
+APP_PATH = f"{DIR_PATH}/app.py"
